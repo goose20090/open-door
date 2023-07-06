73aefb721c83bdbebcc0ff176452a9de698a610c
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-07-06 23:44:37 +0100

    adding build instructions for client

--- bin/open-door-build.sh
+++ bin/open-door-build.sh
@@ -2,8 +2,13 @@
 # exit on error
 set -o errexit
 
-bundle install
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+
+cp -a client/dist. public/
+
 # bundle exec rake assets:precomplie 
 # bundle exec rake assets:clean
+bundle install
 bundle exec rake db:migrate
 bundle exec rake db:seed
